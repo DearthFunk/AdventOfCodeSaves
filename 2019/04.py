97a960955979eac3b23dcbf419d581891f9cf7ac
KillingForm <asdf>
2019-12-19 15:28:50 -0500

    day 4 answer

--- 2019/04.py
+++ 2019/04.py
@@ -1,5 +1,36 @@
+def get_possible_password_values_count(max_len, start, end):
+    def does_value_increment(val):
+        str_val = str(val)
+        for i in range(0, len(str_val) - 1):
+            a = str_val[i]
+            b = str_val[i + 1]
+            if b < a:
+                return False
 
-max_len = 6
-within_range = (356261, 846303)
-# at least 2 adjacent digits are the same (ex: 22 in 122345)
-# L-> R digits never decrease
+        return True
+
+    def does_value_contain_duplicate(val):
+        str_val = str(val)
+        for i in range(0, len(str_val)-1):
+            a = str_val[i]
+            b = str_val[i+1]
+            if a == b:
+                return True
+
+        return False
+
+    total = 0
+    for i in range(start, end + 1):
+        incremental_number = does_value_increment(i)
+        contains_duplicate = does_value_contain_duplicate(i)
+
+        if incremental_number and contains_duplicate:
+            total += 1
+
+    return total
+
+
+result = get_possible_password_values_count(6, 387638, 919123)
+print(result)
+
+
